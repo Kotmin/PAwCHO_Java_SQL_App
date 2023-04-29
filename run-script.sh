f321e0cf6a1d8961fc3f78545c8d1e0df1efd5c1
Kotmin <70173732+Kotmin@users.noreply.github.com>
2023-04-29 20:14:54 +0200

    fix name bug

--- run-script.sh
+++ run-script.sh
@@ -4,12 +4,12 @@
 docker build . -t my_sql_server_for_java -f mysql/Dockerfile
 docker run -p 3306:3306 -d my_sql_server_for_java --rm
 
-docker build . -t java_console_CRUD_app
+docker build . -t java_console_crud_app
 
-docker run -it --tty java_console_CRUD_app
+docker run -it --tty java_console_crud_app
 
 
 docker stop my_sql_server_for_java
-docker stop java_console_CRUD_app
+docker stop java_console_crud_app
 
-echo "Script succesfully ended"
+echo "Script ended"
