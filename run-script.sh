d486369fbd69607c28300fb028b0ea008e7963fa
Kotmin <70173732+Kotmin@users.noreply.github.com>
2023-05-07 12:09:24 +0200

    add networking to script

--- run-script.sh
+++ run-script.sh
@@ -2,11 +2,11 @@
 
 
 docker build . -t my_sql_server_for_java -f mysql/Dockerfile
-docker run -p 3306:3306 -d --name my_db --rm my_sql_server_for_java 
+docker run -p 3306:3306 -d --name my_db --network my_bridge_network --rm my_sql_server_for_java 
 
 docker build . -t java_console_crud_app
 
-docker run -it --tty --name java_console_crud_app --rm java_console_crud_app
+docker run -it --tty --name java_console_crud_app --network my_bridge_network --rm java_console_crud_app
 
 
 docker stop my_db
