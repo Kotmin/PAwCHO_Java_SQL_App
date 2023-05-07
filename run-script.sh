4c33daf1667b6e834c32532713e994ae698c43b4
Kotmin <70173732+Kotmin@users.noreply.github.com>
2023-05-07 12:12:55 +0200

    create network

--- run-script.sh
+++ run-script.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+docker network create my_bridge_network
 
 docker build . -t my_sql_server_for_java -f mysql/Dockerfile
 docker run -p 3306:3306 -d --name my_db --network my_bridge_network --rm my_sql_server_for_java 
 
 docker build . -t java_console_crud_app
 
-docker run -it --tty --name java_console_crud_app --network my_bridge_network --rm java_console_crud_app
+docker run -it --name java_console_crud_app --network my_bridge_network --rm java_console_crud_app
 
 
 docker stop my_db
 docker stop java_console_crud_app
+
+docker network rm my_bridge_network
 
 echo "Script ended"
