3ab830fac8868f1aa35c04c6c9c8b3ef37490610
Kotmin <70173732+Kotmin@users.noreply.github.com>
2023-05-07 20:59:51 +0200

    p flag removeall

--- run-script.sh
+++ run-script.sh
@@ -3,7 +3,7 @@
 docker network create my_bridge_network
 
 docker build . -t my_sql_server_for_java -f mysql/Dockerfile
-docker run -p 3306:3306 -d --name my_db --network my_bridge_network --rm my_sql_server_for_java 
+docker run -d --name my_db --network my_bridge_network --rm my_sql_server_for_java 
 
 docker build . -t java_console_crud_app
 
